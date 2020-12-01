c45c4f63d5f02a4d7e952b9f349154ddca571cd6
Caldum <nachocaldumbide@gmail.com>
2020-12-01 16:24:34 -0300

    adds input to Index component

--- components/Index.jsx
+++ components/Index.jsx
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { getData } from '../redux/actions';
 
-export default function Index () {
+export function Index ({ getData, data }) {
   const [URL, onChangeURL] = useState('');
+
+  const handleRequest = () => {
+    console.log('holu');
+    getData(URL);
+  };
 
   return (
     <View style={styles.container}>
@@ -12,8 +19,9 @@ export default function Index () {
         <Text style={styles.title}>FilesTree Visualizer</Text>
       </View>
       <View>
-        <Text style={styles.label}>URL de datos:</Text>
+        <Text style={styles.label}>Data URL:</Text>
         <TextInput style={styles.input} autoCapitalize="none" value={URL} onChangeText={text => onChangeURL(text)} placeholder="https://ejemplo.com/api" />
+        <Button onPress={() => handleRequest()} title="Realizar consulta" />
       </View>
     </View>
   );
@@ -41,3 +49,17 @@ const styles = StyleSheet.create({
     borderWidth: 1
   }
 });
+
+function mapStateToProps (state) {
+  return {
+    data: state.visualizer.data
+  };
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    getData: (url) => dispatch(getData(url))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
