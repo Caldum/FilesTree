e99cbfae169ad3cfbd63c1506f5a93a61e3b228a
Caldum <nachocaldumbide@gmail.com>
2020-12-02 17:12:24 -0300

    modifies component Index, modifies redux

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 export const GET_DATA = 'GET_DATA';
+export const ERROR = 'ERROR';
 
 export function getData (url) {
   return dispatch => {
     return axios.get(url)
       .then(res => {
         dispatch({ type: GET_DATA, payload: res.data });
+      })
+      .catch(() => {
+        dispatch({ type: ERROR });
       });
   };
 }
